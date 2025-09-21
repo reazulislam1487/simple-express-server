485442514ac509817a6d266782684e4ab145d9c1
Reazul Islam Reaz <reazulislam1487@gmail.com>
2025-09-21 23:58:42 +0600

    added all code

---
+++ app.js
@@ -0,0 +1,43 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.set("view engine", "ejs");
+
+const admin = express.Router();
+admin.get("/", (req, res) => {
+  res.send("Admin");
+});
+
+app.param("id", (req, res, next, id) => {
+  const userName = req.body.name;
+  const user = {
+    id,
+    userName,
+  };
+  req.userinfo = user;
+  next();
+});
+
+app.use("/admin", admin);
+app.get("/", (req, res) => {
+  // res.json("response done");
+  res.render("text/index", {
+    name: "Reazul Islam Reaz",
+  });
+});
+
+app.get("/:id", (req, res) => {
+  const userDetails = req.userinfo;
+  console.log(userDetails);
+  res.json({ userDetails });
+});
+app.post("/", (req, res) => {
+  const data = req.body;
+  console.log(data.courseId);
+  res.json(data);
+});
+app.listen(3000, () => {
+  console.log("Express Server is running on http://localhost:3000");
+});
